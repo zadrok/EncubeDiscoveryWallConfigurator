142497daeec3db3b6e410c8e6901e21406c83d93
Lennon Jones <lennons@gmail.com>
2018-04-29 18:22:20 +1000

    added to .gitignore

--- gui.py
+++ gui.py
@@ -1,14 +1,15 @@
 import tkinter as tk
 
 from screen import Screen
-from panel import Panel
+
 
 class GUI(tk.Frame):
-    def __init__(self,model,jsonHandler,root):
+    def __init__(self, model, jsonHandler, root):
         super().__init__(root)
         self.model = model
         self.jsonHandler = jsonHandler
         self.root = root
+        self.screens = []
         self.grid()
         self.create_widgets()
 
@@ -18,8 +19,42 @@ class GUI(tk.Frame):
         self.hi_there["command"] = self.say_hi
         self.hi_there.grid(row=0, column=0)
 
+        self.add_screen = tk.Button(self)
+        self.add_screen["text"] = "Add Screen"
+        self.add_screen["command"] = self.create_screen
+        self.add_screen.grid(row=0, column=1)
+
+        self.set_up_screen_area()
+
         self.quit = tk.Button(self, text="QUIT", fg="red", command=self.root.destroy)
         self.quit.grid(row=1, column=0)
 
     def say_hi(self):
         print("hi there, everyone!")
+
+    def create_screen(self):
+
+        # Creating a new screen.
+        s = Screen("id", 0, 0)
+        self.screens.append(s)
+
+        # Calculate new size of screens.
+        count_screens = len(self.screens)
+        height = self.root.winfo_height()
+        width = (self.root.winfo_width() / count_screens) if (count_screens > 0) else self.root.winfo_width()
+
+        for sc in self.screens:
+            sc.set_width(width)
+            sc.set_height(height)
+            sc.set_id(sc.get_name())
+            sc.update_pane()
+
+        for sc in self.screens:
+            self.paned.remove(sc.get_pane())
+
+        for sc in self.screens:
+            self.paned.add(sc.get_pane(), stretch="always")
+
+    def set_up_screen_area(self):
+        self.paned = tk.PanedWindow(orient="horizontal", width=1280, height=720)
+        self.paned.grid(row=2, column=0)
--- screen.py
+++ screen.py
@@ -1,6 +1,29 @@
+import tkinter as tk
 
 class Screen:
-    def __init__(self,id,width=100,height=100):
-        self.id = id
+    def __init__(self, id="id", width=100, height=100):
+        self.name = id
         self.width = width
         self.height = height
+        self.id = self.name + " : " + str(self.width) + " x " + str(self.height)
+        self.pane = tk.LabelFrame(text=self.id, padx=self.height, pady=self.width)
+
+    def get_pane(self):
+        return self.pane
+
+    def get_name(self):
+        return self.name
+
+    def update_pane(self):
+        self.pane.config(height=self.height)
+        self.pane.config(width=self.width)
+        self.pane.config(text=self.id)
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def set_id(self, id):
+        self.id = id + ": " + str(self.width) + " x " + str(self.height)
