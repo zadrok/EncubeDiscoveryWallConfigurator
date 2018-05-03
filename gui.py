3df3e694689f2e89909bb9da932a08841658f488
Kevin M <kevin@dcode.com.au>
2018-05-03 12:02:41 +1000

    basic stuff for dividing screens in canvas

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
-
+import random
 from screen import Screen
-from panel import Panel
+
 
 class GUI(tk.Frame):
     def __init__(self,model,jsonHandler,root):
@@ -10,16 +10,41 @@ class GUI(tk.Frame):
         self.jsonHandler = jsonHandler
         self.root = root
         self.grid()
+        self.screens = []
+        self.quit = None
+        self.add_screen = None
+        self.canvas = None
+        # self.canvas_w = self.root.winfo_windowwidth()
+        # self.canvas_h = self.root.winfo_windowheight()
+        self.canvas_w = 1280
+        self.canvas_h = 720
         self.create_widgets()
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.grid(row=0, column=0)
-
         self.quit = tk.Button(self, text="QUIT", fg="red", command=self.root.destroy)
-        self.quit.grid(row=1, column=0)
+        self.quit.pack()
 
-    def say_hi(self):
-        print("hi there, everyone!")
+        self.add_screen = tk.Button(self, text="Add Screen", command=self.create_screen)
+        self.add_screen.pack()
+
+        self.canvas = tk.Canvas(self, width=self.canvas_w, height=self.canvas_h, bg="blue")
+        self.canvas.pack()
+
+    def create_screen(self):
+        s = Screen(str(len(self.screens)), self.root.winfo_width(), self.root.winfo_height())
+        self.screens.append(s)
+        count_screens = len(self.screens)
+        print(str(count_screens) + " screens")
+        self.canvas.delete("all")
+
+
+        for i, s in enumerate(self.screens):
+            x = (self.canvas_w / count_screens) * i if (i > 0) else 0
+            y = 0
+            w = x * 2 if ( x > 0) else self.canvas_w
+            h = 720
+            color = self.random_color()
+            self.canvas.create_rectangle(x, y, w, h, width=3, fill=color)
+
+    def random_color(self):
+        return '#%02X%02X%02X' % (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
--- screen.py
+++ screen.py
@@ -1,6 +1,6 @@
 
 class Screen:
-    def __init__(self,id,width=100,height=100):
+    def __init__(self, id, width=100, height=100):
         self.id = id
         self.width = width
         self.height = height
