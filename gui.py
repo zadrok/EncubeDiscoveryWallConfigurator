72a6fb8f107cc0c3780cdf69ed8a0ebb7d584efe
Kevin M <kevin@dcode.com.au>
2018-05-03 12:42:08 +1000

    code cleanup

--- gui.py
+++ gui.py
@@ -14,8 +14,6 @@ class GUI(tk.Frame):
         self.quit = None
         self.add_screen = None
         self.canvas = None
-        # self.canvas_w = self.root.winfo_windowwidth()
-        # self.canvas_h = self.root.winfo_windowheight()
         self.canvas_w = 1280
         self.canvas_h = 720
         self.create_widgets()
@@ -31,20 +29,19 @@ class GUI(tk.Frame):
         self.canvas.pack()
 
     def create_screen(self):
-        s = Screen(str(len(self.screens)), self.root.winfo_width(), self.root.winfo_height())
+        s = Screen(self.canvas, "Screen", 0, 0, 0, 0, self.random_color())
         self.screens.append(s)
         count_screens = len(self.screens)
-        print(str(count_screens) + " screens")
-        self.canvas.delete("all")
 
+        self.canvas.delete("all")
 
         for i, s in enumerate(self.screens):
             x = (self.canvas_w / count_screens) * i if (i > 0) else 0
             y = 0
-            w = x * 2 if ( x > 0) else self.canvas_w
+            w = x * 2 if (x > 0) else self.canvas_w
             h = 720
-            color = self.random_color()
-            self.canvas.create_rectangle(x, y, w, h, width=3, fill=color)
+            s.set_position(x, y, w, h)
+            s.draw()
 
     def random_color(self):
         return '#%02X%02X%02X' % (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
--- screen.py
+++ screen.py
@@ -1,6 +1,62 @@
 
 class Screen:
-    def __init__(self, id, width=100, height=100):
-        self.id = id
+    def __init__(self, canvas, ident, x, y, width, height, color):
+        self.canvas = canvas
+        self.id = ident
+        self.x = x
+        self.y = y
         self.width = width
         self.height = height
+        self.color = color
+
+    def draw(self):
+        self.canvas.create_rectangle(
+            self.x,
+            self.y,
+            self.width,
+            self.height,
+            width=3,
+            fill=self.color
+        )
+
+    def set_position(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+
+    def get_id(self):
+        return self.id
+    
+    def set_id(self, ident):
+        self.id = ident
+
+    def get_x(self):
+        return self.x
+
+    def set_x(self, x):
+        self.x = x
+
+    def get_y(self):
+        return self.y
+
+    def set_y(self, y):
+        self.y = y
+
+    def get_width(self):
+        return self.width
+
+    def set_width(self, width):
+        self.width = width
+
+    def get_height(self):
+        return self.height
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_canvas(self):
+        return self.canvas
+
+    def set_canvas(self, canvas):
+        self.canvas = canvas
