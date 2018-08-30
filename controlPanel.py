d1cb7d078396969e479b7fd1c3317b91ddf0f910
Jihoon Woo <Jihoonwoo6189@gmail.com>
2018-08-30 16:14:28 +1000

    Clean code
    Clean code and add comments

--- controlPanel.py
+++ controlPanel.py
@@ -15,59 +15,90 @@ class controlPanel():
         self.frame = tk.Frame(self.secondaryWindow)
         self.frame.pack(side="top")
 
-        # One of children frame to store save and load buttons
-        self.saveAndLoadFrame = tk.Frame(self.frame, highlightbackground="black", highlightthickness=1, width=200, height=55)
+        # Frame for Save and Load
+        self.saveAndLoadFrame = tk.Frame(self.frame, highlightbackground="black", highlightthickness=1, width=200)
         self.saveAndLoadFrame.pack(side="left")
+        # Frame for Save and Load Label
+        self.saveAndLoadLabelFrame = tk.Frame(self.saveAndLoadFrame, background = "#8583D7" )
+        self.saveAndLoadLabelFrame.pack(side="top", fill="both")
+        # Save and Load Label
+        self.saveAndLoadLabel = tk.Label(self.saveAndLoadLabelFrame, text="File", height=2, background = "#8583D7" )
+        self.saveAndLoadLabel.pack(side="left", padx = (4,6))
+        # Save and Load buttons
+        self.secondaryWindow.saveButton = tk.Button(self.saveAndLoadFrame, text="Save", command=self.DecreaseSplitNum, activebackground="white")
+        self.secondaryWindow.saveButton.pack(side="left", padx=20)
+        self.secondaryWindow.loadButton = tk.Button(self.saveAndLoadFrame, text="Load", command=self.DecreaseSplitNum, activebackground="white")
+        self.secondaryWindow.loadButton.pack(side="left", padx=20)
 
-        #One of children frame to store split buttons
-        self.splitButtonFrame = tk.Frame(self.frame, highlightbackground="black", highlightthickness=1, width=1250, height=55, bd=0)
+        # Frame for Split buttons
+        self.splitButtonFrame = tk.Frame(self.frame, highlightbackground="black", highlightthickness=1, width=1250)
         self.splitButtonFrame.pack(side="left")
-        #nested frame in splitButtonFrame in order to gives different rows
-        self.splitButtonFrame2 = tk.Frame(self.splitButtonFrame)
-        self.splitButtonFrame2.pack(side="top")
-
-        self.splitNumberLabel = tk.Label(self.splitButtonFrame2, text="Split Panel")
-        self.splitNumberLabel.pack(side="left")
-
-
-        #the other frame to store change pannel mode buttons
-        self.ChangePannelModeFrame = tk.Frame(self.frame, highlightbackground="black", highlightthickness=1, width=800, height=55)
-        self.ChangePannelModeFrame.pack(side="right")
-
-        #Decrease Split number button, using Unicode to create Arrow
-        self.secondaryWindow.DecreaseSplitNumberButton = tk.Button(self.splitButtonFrame2, text=u"\u2B9C", command=self.DecreaseSplitNum, activebackground="white")
-        self.secondaryWindow.DecreaseSplitNumberButton.pack(side="left", padx=20)
+        # Frame for Split buttons Label
+        self.splitButtonLabelFrame = tk.Frame(self.splitButtonFrame, background = "#8583D7")
+        self.splitButtonLabelFrame.pack(side="top", fill="both")
+        # Label for split buttons frame
+        self.splitNumberLabel = tk.Label(self.splitButtonLabelFrame, text="Split Panel", height = 2, background = "#8583D7")
+        self.splitNumberLabel.pack(side="left", fill="both", padx = (4,6))
+        # Decrease button
+        self.secondaryWindow.DecreaseSplitNumberButton = tk.Button(self.splitButtonFrame, text=u"\u21e6", command=self.DecreaseSplitNum, activebackground="white")
+        self.secondaryWindow.DecreaseSplitNumberButton.pack(side="left")
         # Display split number
         self.splitNumberString = tk.StringVar()
         self.splitNumber = 2
-        self.splitNumberLabel = tk.Label(self.splitButtonFrame2, textvariable= self.splitNumberString)
+        self.splitNumberLabel = tk.Label(self.splitButtonFrame, textvariable=self.splitNumberString, background="white")
         self.splitNumberLabel.pack(side="left")
-        self.splitNumberString.set("Split Number : "+str(self.splitNumber))
-        # Increase Split number button, using Unicode to create Arrow
-        self.secondaryWindow.IncreaseSplitNumberButton = tk.Button(self.splitButtonFrame2,  text=u"\u2B9E", command=self.IncreaseSplitNum, activebackground="white")
-        self.secondaryWindow.IncreaseSplitNumberButton.pack(side="left", padx=20)
-
-        self.secondaryWindow.HorizonSplitButton = tk.Button(self.splitButtonFrame, text ="H-split", command=self.HSplit, activebackground = "white")
-        self.secondaryWindow.HorizonSplitButton.pack(side="left",padx= 20 )
+        self.splitNumberString.set("Split Number : " + str(self.splitNumber))
+        # Increase button
+        self.secondaryWindow.IncreaseSplitNumberButton = tk.Button(self.splitButtonFrame, text=u"\u21e8", command=self.IncreaseSplitNum, activebackground="white")
+        self.secondaryWindow.IncreaseSplitNumberButton.pack(side="left")
+        # Horizon Split button
+        self.secondaryWindow.HorizonSplitButton = tk.Button(self.splitButtonFrame, text="H-split", command=self.HSplit, activebackground="white")
+        self.secondaryWindow.HorizonSplitButton.pack(side="left", padx=20)
+        # Vertical Split button
         self.secondaryWindow.VerticalSplitButton = tk.Button(self.splitButtonFrame, text="V-split", command=self.VSplit)
-        self.secondaryWindow.VerticalSplitButton.pack(side="left",padx=20)
+        self.secondaryWindow.VerticalSplitButton.pack(side="left", padx=20)
+        # Join panel Button
         self.secondaryWindow.JoinPannelButton = tk.Button(self.splitButtonFrame, text="Join", command=self.JoinPannel)
-        self.secondaryWindow.JoinPannelButton.pack(side="left",padx=20)
+        self.secondaryWindow.JoinPannelButton.pack(side="left", padx=20)
+        # Remove panel Button
         self.secondaryWindow.RemovePannelButton = tk.Button(self.splitButtonFrame, text="Remove", command=self.RemovePannel)
-        self.secondaryWindow.RemovePannelButton.pack(side="left",padx=20)
+        self.secondaryWindow.RemovePannelButton.pack(side="left", padx=20)
+        # Delete panel button
         self.secondaryWindow.DeselectPannelButton = tk.Button(self.splitButtonFrame, text="Deselect", command=self.DeselectPannel)
-        self.secondaryWindow.DeselectPannelButton.pack(side="left",padx=20)
+        self.secondaryWindow.DeselectPannelButton.pack(side="left", padx=20)
+        # Select All panel button
         self.secondaryWindow.SelectAllPannelButton = tk.Button(self.splitButtonFrame, text="Select All", command=self.SelectAllPannel)
-        self.secondaryWindow.SelectAllPannelButton.pack(side="left",padx=20)
+        self.secondaryWindow.SelectAllPannelButton.pack(side="left", padx=20)
+        # Reset panel button
         self.secondaryWindow.ResetPannelButton = tk.Button(self.splitButtonFrame, text="Reset", command=self.Reset)
         self.secondaryWindow.ResetPannelButton.pack(side="left", padx=20)
 
+
+        #the other frame to store change pannel mode buttons
+        self.ChangePannelModeFrame = tk.Frame(self.frame, highlightbackground="black", highlightthickness=1, width=800)
+        self.ChangePannelModeFrame.pack(side="left")
+        # the other frame to store change pannel mode buttons
+        self.ChangePannelModeLabelFrame = tk.Frame(self.ChangePannelModeFrame, width=800, background = "#8583D7")
+        self.ChangePannelModeLabelFrame.pack(side="top", fill="both")
+        # Label for change panel mode buttons frame
+        self.ChangePanelModeLabel = tk.Label(self.ChangePannelModeLabelFrame, text="Panel mode", height = 2, background = "#8583D7")
+        self.ChangePanelModeLabel.pack(side="left", padx = (4,6))
+        # Change panel mode to Encube button
+        self.secondaryWindow.ChangePannelModeCubeLabel = tk.Label(self.ChangePannelModeFrame, text = "", width= 5, background="#00FFFF")
+        self.secondaryWindow.ChangePannelModeCubeLabel.pack(side="left",padx=(20,0))
         self.secondaryWindow.ChangePannelModeCube = tk.Button(self.ChangePannelModeFrame, text="Cube", command=self.DeselectPannel)
-        self.secondaryWindow.ChangePannelModeCube.pack(side="left", padx=20)
+        self.secondaryWindow.ChangePannelModeCube.pack(side="left", padx=(0,20))
+        # Change panel mode to Image button
+        self.secondaryWindow.ChangePannelModeImageLabel = tk.Label(self.ChangePannelModeFrame, text="", width=5, background="#e500ff")
+        self.secondaryWindow.ChangePannelModeImageLabel.pack(side="left")
         self.secondaryWindow.ChangePannelModeImage = tk.Button(self.ChangePannelModeFrame, text="Image", command=self.SelectAllPannel)
-        self.secondaryWindow.ChangePannelModeImage.pack(side="left", padx=20)
+        self.secondaryWindow.ChangePannelModeImage.pack(side="left",padx=(0,20))
+        # Change panel mode to Graph button
+        self.secondaryWindow.ChangePannelModeGraphLabel = tk.Label(self.ChangePannelModeFrame, text="", width=5, background="#f8ff47")
+        self.secondaryWindow.ChangePannelModeGraphLabel.pack(side="left")
         self.secondaryWindow.ChangePannelModeGraph = tk.Button(self.ChangePannelModeFrame, text="Graph", command=self.Reset)
-        self.secondaryWindow.ChangePannelModeGraph.pack(side="left", padx=20)
+        self.secondaryWindow.ChangePannelModeGraph.pack(side="left",padx=(0,20))
+
 
     def DecreaseSplitNum(self):
         if self.splitNumber > 2:
